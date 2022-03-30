fe3dc6aa1b8d2908d544b90c73d20ea52c44104e
epanshop4 <102691701+epanshop4@users.noreply.github.com>
2022-03-31 01:38:39 +0700

    Update donasi.js

--- src/donasi.js
+++ src/donasi.js
@@ -1,10 +1,10 @@
 exports.donasi = (prefix) => {
 return `*- DONASI*
 
-• Gopay
-• Dana
-• Ovo
-• Qris allpay
+• Gopay 62882-0063-77070
+• Dana 62882-0063-77070
+• Ovo 62882-0063-77070
+• Qris allpay _Minta owner_
 
 _Untuk info selanjutnya ketik_ *${prefix}payment*
 _Thanks buat kalian yang sudah donasi_`
